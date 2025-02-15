48ebd18eca4718ee06245f368326b38d66995940
Aniket <aniketparida666@gmail.com>
2025-02-15 22:09:40 +0530

    To do list

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-const count = document.getElementById('count');
-let time = null;
-let startTime = 0;
-let elapsedTime = 0;
-let isRunning = false;
+const content = document.getElementById('content');
+const getResult = document.getElementById('input-item');
 
-const Start = () => {
-	if (!isRunning) {
-		isRunning = true;
-		startTime = Date.now() - elapsedTime;
-		time = setInterval(update, 10);
+function addItem() {
+	const newItem = content.value;
+	if (newItem === '') {
+		alert('Input the Text');
+		return;
+	} else {
+		const li = document.createElement('li');
+		li.className =
+			'w-85 h-8 border-1 border-black rounded-md p-0 ml-5 mt-3 inline-flex content-center justify-between font-bold';
+		li.innerHTML = `
+            ${newItem}
+            <button type="button" class="text-red-700 text-2xl" onclick="removeItem(this)">
+                <i class="bx bx-x mb-1"></i>
+            </button>
+        `;
+		getResult.appendChild(li);
+		content.value = '';
 	}
-};
+}
 
-const Stop = () => {
-	if (isRunning) {
-		isRunning = false;
-		clearInterval(time);
-	}
-};
-
-const Restart = () => {
-	isRunning = false;
-	clearInterval(time);
-	elapsedTime = 0;
-	count.textContent = '00:00:00:00';
-};
-
-const update = () => {
-	const currentTime = Date.now();
-	elapsedTime = currentTime - startTime;
-
-	let hour = Math.floor(elapsedTime / (1000 * 60 * 60));
-	let minute = Math.floor((elapsedTime / (1000 * 60)) % 60);
-	let second = Math.floor((elapsedTime / 1000) % 60);
-	let miliSecond = Math.floor((elapsedTime % 1000) / 10);
-
-	count.textContent = `${hour.toString().padStart(2, '0')}-${minute
-		.toString()
-		.padStart(2, '0')}-${second.toString().padStart(2, '0')}-${miliSecond
-		.toString()
-		.padStart(2, '0')}`;
-};
+function removeItem(button) {
+	const li = button.parentElement;
+	li.remove();
+}
